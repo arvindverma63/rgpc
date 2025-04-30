c8deb7a30450d2e7cd42335669b3bca84c0359a2
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-04-30 13:16:58 +0530

    update

--- components/head.php
+++ components/head.php
@@ -11,7 +11,6 @@
 		<meta name="keywords" content="">
 		<!-- meta character set -->
 		<meta charset="UTF-8">
-		<!-- Site Title -->
 		<title>Education</title>
 
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
